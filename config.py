3d547c85ddf88a5850a8b0f4eb3f7098696046f8
𝐒𝐔𝐃𝐄𝐄𝐏𝐁𝐎𝐓𝐒 <sudeeptelegram@gmail.com>
2025-12-22 23:33:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,14 +5,8 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 MONGO_URL = os.getenv("MONGO_URL")
 OWNER_ID = 6356015122  # Tumhara (Owner) Telegram ID
 
-# 🔥 MUSIC BOT CONFIG (Assistant)
-# Ye teeno .env file se aayenge
-API_ID = int(os.getenv("API_ID", "0")) # Integer hona zaroori hai
-API_HASH = os.getenv("API_HASH")
-SESSION_STRING = os.getenv("SESSION_STRING")
-
 # 🤖 AI CHAT CONFIG
-OWNER_NAME = "ᯓ𓂃❛ 𝐒 𝛖 𝐝 ֟፝ᥱ 𝛆 𝛒 </𝟑 𝁘ໍ𝀛𓂃🍷"  # Yuki tumhe is naam se bulayegi
+OWNER_NAME = "ᯓ𓂃❛ 𝐒 𝛖 𝐝 ֟፝ᥱ 𝛆 𝛒 </𝟑 𝁘ໍ𝀛𓂃🍷"  # Yahan apna naam likho (Yuki tumhe is naam se bulayegi)
 
 # 🎮 GAME SETTINGS
 GRID_SIZE = 4
@@ -21,4 +15,6 @@ LOAN_INTEREST = 0.10
 DELETE_TIMER = 17  # Result message kitne seconds baad delete hoga
 
 # 🏆 RANKING IMAGE
+# Agar top group ki photo nahi milti toh ye default image dikhegi
 DEFAULT_BANNER = "https://i.ibb.co/vzDpQx9/ranking-banner.jpg"
+
